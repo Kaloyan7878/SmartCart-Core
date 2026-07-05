25302a600fa401d3e7b89e3c7dd4a1423934f1e6
Kaloyan7878 <kaloyaniv05@abv.bg>
2026-07-06 00:22:06 +0300

    Add household_list, household_detail, add_household views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def household_list(request):
+    memberships = HouseholdMembership.objects.filter(user=request.user)
+    return render(request, "accounts/household_list.html", {"memberships": memberships})
+
+def add_household(request):
+    if request.method == 
+
+
+def edit_budget
